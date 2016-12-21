16294bc8822aed0a8233322c9b2e42e3bc58ab53
The-BFG <guerzonigiacom95@gmail.com>
2016-12-21 15:48:48 +0100

    modified BMTableModel.java

--- BudgetManagement.java
+++ BudgetManagement.java
@@ -5,22 +5,22 @@ import BM.BMTableModel;
 //import javax.swing.JFrame;
 
 public class BudgetManagement {
-	public static void main(String[] args) {
-            JFrame mainFrame = new JFrame("Budget Management");
-            mainFrame.setLocation(100, 20);
-            mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          
-            JLabel lTest = new JLabel("porca troietta");
-            
-            BMTableModel tableModel = new BMTableModel();
-            JTable table = new JTable(tableModel);
-            JScrollPane scrollPane = new JScrollPane(table);
-            
-            JPanel mainPanel = (JPanel) mainFrame.getContentPane();//come se fosse un puntatore al panel predefinito del JFrame
-            mainPanel.add(scrollPane); 
-            //mainFrame.add(mainPanel);
-            
-            mainFrame.pack();
-            mainFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Budget Management");
+        mainFrame.setLocation(100, 20);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel lTest = new JLabel("porca troietta");
+
+        BMTableModel tableModel = new BMTableModel();
+        JTable table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        JPanel mainPanel = (JPanel) mainFrame.getContentPane();//come se fosse un puntatore al panel predefinito del JFrame
+        mainPanel.add(scrollPane); 
+        //mainFrame.add(mainPanel);
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
 }
--- src/BM/BMTableModel.java
+++ src/BM/BMTableModel.java
@@ -1,24 +1,29 @@
 import javax.swing.table.AbstractTableModel;
 import java.util.List;
 
-public class BMTabelModel extends AbstractTableModel {
-    List ;
-    String[] ColName = {"Date","Description","Amount","Remove","Modify"};
+public class BMTableModel extends AbstractTableModel {
+    List transactions = null;
+    String[] colName = {"Date","Description","Amount","Remove","Modify"};
+    
+    public BMTableModel(List transactions) {
+        this.transactions = transactions;
+    }
     public int getColumnCount() {
-    
-    };
+        return colName.length;
+    }
     public int getRowCount() {
-    
-    };
-    public int getValueAt() {
-    
-    };
+        return transactions.size();
+    }
+    public Object getValueAt(int row, int col) {
+        BMItem item = (BMItem)transactions.get(row);
+        
+    }
     
     public String getColumnName(int col) {
         
-    };
+    }
     
     public boolean idCellEditable(int row, int col) {
         
-    };
+    }
 }
