28f362e5a488d129de92f333b7e99331d8f74356
The-BFG <guerzonigiacom95@gmail.com>
2016-12-21 14:56:56 +0100

    modified directory hierarchy

--- BudgetManagement.java
+++ BudgetManagement.java
@@ -1,7 +1,26 @@
-public class BudgetManagement
-{
-	public static void main(String[] args)
-	{
-        System.out.println("Dio Lercio");
+//import java.awt.*;
+import javax.swing.*;
+import javax.swing.JTable;
+import BM.BMTableModel;
+//import javax.swing.JFrame;
+
+public class BudgetManagement {
+	public static void main(String[] args) {
+            JFrame mainFrame = new JFrame("Budget Management");
+            mainFrame.setLocation(100, 20);
+            mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          
+            JLabel lTest = new JLabel("porca troietta");
+            
+            BMTableModel tableModel = new BMTableModel();
+            JTable table = new JTable(tableModel);
+            JScrollPane scrollPane = new JScrollPane(table);
+            
+            JPanel mainPanel = (JPanel) mainFrame.getContentPane();//come se fosse un puntatore al panel predefinito del JFrame
+            mainPanel.add(scrollPane); 
+            //mainFrame.add(mainPanel);
+            
+            mainFrame.pack();
+            mainFrame.setVisible(true);
 	}
 }
---
+++ src/BM/BMTableModel.java
@@ -0,0 +1,24 @@
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class BMTabelModel extends AbstractTableModel {
+    List ;
+    String[] ColName = {"Date","Description","Amount","Remove","Modify"};
+    public int getColumnCount() {
+    
+    };
+    public int getRowCount() {
+    
+    };
+    public int getValueAt() {
+    
+    };
+    
+    public String getColumnName(int col) {
+        
+    };
+    
+    public boolean idCellEditable(int row, int col) {
+        
+    };
+}
