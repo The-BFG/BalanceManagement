6bdb338b7450aa659c26bc57d88618a02a959c22
The-BFG <guerzonigiacom95@gmail.com>
2016-12-28 12:48:00 +0100

    layout modified

--- src/BM/BMPeriodPanel.java
+++ src/BM/BMPeriodPanel.java
@@ -1,8 +1,12 @@
 package BM;
 
+import java.awt.Component;
+import java.awt.Dimension;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Properties;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.jdatepicker.impl.JDatePanelImpl;
@@ -16,10 +20,12 @@ public class BMPeriodPanel extends JPanel {
     private final UtilDateModel model;
     private final Properties p;
     private JDatePanelImpl datePanel;
-    private JDatePickerImpl datePicker;
+    private JDatePickerImpl fromDatePicker, toDatePicker;
+    private BoxLayout panelLayout, midiLayout;
+    private JPanel midiPanel;
     
     public BMPeriodPanel() {
-        
+        periodLbl.setAlignmentX(Component.LEFT_ALIGNMENT);
         model = new UtilDateModel();
         model.setDay((new GregorianCalendar().get(Calendar.DAY_OF_MONTH)));
         model.setMonth((new GregorianCalendar().get(Calendar.MONTH)));
@@ -29,10 +35,27 @@ public class BMPeriodPanel extends JPanel {
         p.put("text.month", "Month");
         p.put("text.year", "Year");
         datePanel = new JDatePanelImpl(model, p);
-        datePicker = new JDatePickerImpl(datePanel, new BMDateFormatter());
+        fromDatePicker = new JDatePickerImpl(datePanel, new BMDateFormatter());
+        toDatePicker = new JDatePickerImpl(datePanel, new BMDateFormatter());
+
+        
+        panelLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(panelLayout);
+        
+        midiPanel = new JPanel();
+        midiLayout = new BoxLayout(midiPanel, BoxLayout.X_AXIS);
+        midiPanel.setLayout(midiLayout);
+        midiPanel.add(fromLbl);
+        midiPanel.add(fromDatePicker);
+        midiPanel.add(Box.createRigidArea(new Dimension(10,0)));
+        midiPanel.add(toLbl);
+        midiPanel.add(toDatePicker);
+        midiPanel.add(Box.createRigidArea(new Dimension(300,0)));
+
         
         add(periodLbl);
-        add(datePicker);
+        add(Box.createRigidArea(new Dimension(0,10)));
+        add(midiPanel);
     }
     
 }
--- src/BM/BMTablePanel.java
+++ src/BM/BMTablePanel.java
@@ -5,18 +5,19 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class BMTablePanel extends JPanel {
+public class BMTablePanel extends JScrollPane {
     //private ArrayList<BMItem> list = new ArrayList<>();
     private BMTableModel tableModel; 
-    
+    private JTable table;
     public BMTablePanel() {
+        super();
         BMItem trans1 = new BMItem(new GregorianCalendar(), "Prova1", 100.1);
         BMItem trans2 = new BMItem(new GregorianCalendar(), "Prova2", 20.1);
 
         tableModel = new BMTableModel();
         tableModel.addItem(trans1);
         tableModel.addItem(trans2);
-        JTable table = new JTable(tableModel);
+        table = new JTable(tableModel);
 
         table.getColumnModel().getColumn(0).setPreferredWidth(200);
         table.getColumnModel().getColumn(1).setPreferredWidth(500);//table.getColumnModel().getColumn(1).getPreferredWidth()+100
@@ -24,7 +25,9 @@ public class BMTablePanel extends JPanel {
         table.getColumnModel().getColumn(3).setPreferredWidth(150);
         table.getColumnModel().getColumn(4).setPreferredWidth(150);
         
-        JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
+        this.getViewport().add(table);
+        //JScrollPane scrollPane = new JScrollPane(table);
+        //add(scrollPane);
+        
     }
 }
