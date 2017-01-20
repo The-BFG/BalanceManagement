f1383d6d9fa30df895306ede8359502d7c9f3b54
The-BFG <guerzonigiacom95@gmail.com>
2017-01-20 17:10:53 +0100

    some uplode

--- src/BM/BMTablePanel.java
+++ src/BM/BMTablePanel.java
@@ -1,7 +1,18 @@
 package BM;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -9,10 +20,12 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.RowFilter;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableRowSorter; 
 
-public class BMTablePanel extends JPanel {
+public class BMTablePanel extends JPanel implements ActionListener, ListSelectionListener{
     private static final long serialVersionUID = 1L;
     private JScrollPane scrollPane;
     private BorderLayout tablePanelLayout;
@@ -20,10 +33,11 @@ public class BMTablePanel extends JPanel {
     private JTable table;
     private TableRowSorter<BMTableModel> tableSorter;
     
-    private JPanel totalPanel;
-    private BoxLayout totalLayout;
+    private JPanel bottomPanel;
+    private BoxLayout bottomLayout;
     private final JLabel totalLbl = new JLabel("Totale delle transazioni: ");
     private JTextField totalTxt;
+    private JButton editBtn, deleteBtn;
     
     public BMTablePanel() {
         scrollPane = new JScrollPane();
@@ -37,6 +51,7 @@ public class BMTablePanel extends JPanel {
         table = new JTable(tableModel);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         table.getTableHeader().setReorderingAllowed(false);
+        //table.addFocusListener(WHEN_FOCUSED);
         
         table.setRowSorter(tableSorter);  
         tableSorter.setSortable(0, false); 
@@ -55,20 +70,37 @@ public class BMTablePanel extends JPanel {
         
         scrollPane.getViewport().add(table);  
         
-        totalTxt = new JTextField(30);
+        totalTxt = new JTextField(20);
         totalTxt.setEditable(false);
-        totalPanel = new JPanel();
-        totalLayout = new BoxLayout(totalPanel,BoxLayout.X_AXIS);
+        JPanel totalPanel = new JPanel();
+        FlowLayout totalLayout = new FlowLayout(FlowLayout.LEFT);
         totalPanel.setLayout(totalLayout);
-        //totalPanel.add(Box.createRigidArea(new Dimension((table.getColumnModel().getColumn(0).getWidth() + (table.getColumnModel().getColumn(1).getWidth()/2)),0)));
         totalPanel.add(totalLbl);
         totalPanel.add(totalTxt);
+        
+        editBtn = new JButton();
+        editBtn = loadIcon(editBtn,"edit.png");
+        deleteBtn = new JButton();
+        deleteBtn = loadIcon(deleteBtn,"trash.png");
+        JPanel editPanel = new JPanel();
+        BoxLayout editLayout = new BoxLayout(editPanel, BoxLayout.X_AXIS);
+        editPanel.setLayout(editLayout);
+        editPanel.add(editBtn);
+        editPanel.add(Box.createRigidArea(new Dimension(5,0)));
+        editPanel.add(deleteBtn);
+        
+        bottomPanel = new JPanel();
+        bottomLayout = new BoxLayout(bottomPanel,BoxLayout.X_AXIS);
+        bottomPanel.setLayout(bottomLayout);
+        bottomPanel.add(totalPanel);
+        bottomPanel.add(Box.createHorizontalGlue());//Box.createRigidArea(new Dimension(300,0)));
+        bottomPanel.add(editPanel);
         this.refreshTotal();
               
         tablePanelLayout = new BorderLayout();
         this.setLayout(tablePanelLayout);
         this.add(scrollPane, BorderLayout.CENTER);
-        this.add(totalPanel, BorderLayout.SOUTH);       
+        this.add(bottomPanel, BorderLayout.SOUTH);       
     }
     
     public JTable getTable() {
@@ -85,5 +117,34 @@ public class BMTablePanel extends JPanel {
     }  
     public final void refreshTotal() {
         totalTxt.setText(String.format("%.2f", getTotal()));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    private JButton loadIcon(JButton btn, String imageName) {
+        Image img = null;
+        try {
+                img =   ( ImageIO
+                        .read(new FileInputStream(((System.getProperty("user.dir").endsWith("class")) ? "../icon/"+imageName : "./icon/"+imageName))))
+                        .getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+            } 
+        catch (IOException ex) {
+            System.out.println("Impossibile caricare l'icona del searchBtn\n" + ex);
+        }
+        btn.setIcon(new ImageIcon(img));
+        btn.setBackground(null);
+        //btn.setBorder(null);
+        btn.setContentAreaFilled(true);
+        btn.addActionListener(this);
+        btn.setEnabled(false);
+        return btn;
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+      
     }
 }
