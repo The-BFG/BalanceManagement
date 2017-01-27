78cf082b07c49a3903c87f8aebe168bc18021336
The-BFG <guerzonigiacom95@gmail.com>
2017-01-27 14:24:22 +0100

    Adding javadoc

--- src/BM/BMTextField.java
+++ src/BM/BMTextField.java
@@ -10,43 +10,84 @@ import java.awt.event.FocusListener;
 import javax.swing.JTextField;
 
 /**
- *
+ *Classe che estende una JTextField in modo tale da aggiungere alcune funzionalita.
+ * 
+ * <br>In particolare vengono aggiunti i metodi che implementano il FocusListener per gestire quando si clicca
+ * sulla JTextField quando si perde il focus da essa.
  * @author giacomo
  */
 public class BMTextField extends JTextField implements FocusListener  {
     private static final long serialVersionUID = 1L;
     private String firstTxt;
     
+    /**
+     * Costruttore della classe <a href="../BM/BMTextField">BMTextField</a>.
+     * 
+     * Inizializza il testo iniziale vuoto.
+     */
     public BMTextField (){
         super();
         firstTxt = "";
         addFocusListener(this);
     }
+    /**
+     * Costruttore della classe <a href="../BM/BMTextField">BMTextField</a>.
+     * 
+     * Inizializza il testo iniziale con la stringa che gli viene passata come parametro.
+     * @param text testo iniziale della {@link BM.BMTextField}.
+     */
     public BMTextField (String text){
         super(text);
         firstTxt = text;
         addFocusListener(this);
     }
+    
+    /**
+     * Costruttore della classe <a href="../BM/BMTextField">BMTextField</a>.
+     * 
+     * Inizializza il testo iniziale vuoto e setta il numero di lettere visibili.
+     * @param nChar numero di lettere che è possibile visualizzare dalla {@link BM.BMTextField}.
+     */
     public BMTextField (int nChar){
         super(nChar);
         firstTxt = "";
         addFocusListener(this);
     }
+    
+    /**
+     * Costruttore della classe <a href="../BM/BMTextField">BMTextField</a>.
+     * 
+     * <br>Inizializza il testo iniziale con la stringa che gli viene passata come parametro e settare il numero di lettere
+     * visibili.
+     * @param text testo iniziale della {@link BM.BMTextField}.
+     * @param nChar numero di lettere che è possibile visualizzare dalla {@link BM.BMTextField}.
+     */
     public BMTextField (String text, int nChar){
         super(text, nChar);
         firstTxt = text;
         addFocusListener(this);
     }
-    
+    /**
+     * Metodo get per ottenere la stringa inziale presente all'interno della {@link BM.BMTextField}.
+     * 
+     * @return String contenente la stringa di default della {@link BM.BMTextField}.
+     */
     public String getDefaultText() {
         return firstTxt;
     }
 
+    /**
+     * Metodo che viene eseguito quando si prende il focus della {@link BM.BMTextField} e si coccupa di eliminarne il contenuto.
+     */
     @Override
     public void focusGained(FocusEvent e) {
         setText(""); // Empty the text field when it receives focus
     }
     @Override
+    
+    /**
+     * Metodo che si occupa di rimettere il testo di default quando la {@link BM.BMTextField} perde il focus.
+     */
     public void focusLost(FocusEvent e) {      
         if(getText().equals("")) {
             setText(firstTxt);
--- src/EXPORT/ExportTSV.java
+++ src/EXPORT/ExportTSV.java
@@ -9,26 +9,45 @@ import BM.BMItem;
 import java.util.ArrayList;
 
 /**
- *
+ *Classe che implementa i metodi astratti ereditati da {@link EXPORT.AbstractExport}.
+ * 
+ * <br>Qui viene settato come separatore di default il carattere di tabulazione.
  * @author giacomo
  */
 public class ExportTSV extends AbstractExport{
     
+    /**
+     * Costruttore che chiama il costruttore del padre {@link BM.BMTextField} ed inizializza il separatore di default.
+     * @param transactions ArrayList contenente tutte le transazioni che devono essere esportate.
+     */
     public ExportTSV(ArrayList<BMItem> transactions){
         super(transactions);
         super.setSeparator("\t");
     }
 
+    /**
+     * Metodo che ritorna il separatore che viene utilizzato per l'esportazione
+     * @return Stringa contenente il carattere per separare i campi.
+     */
     @Override
     public String getSeparator() {
         return "\t";
     }
     
+    /**
+     * Metodo per ottenere l'estenzione del file che verra generato.
+     * @return String contenente l'estensione del file di esportazione
+     */
     @Override
     public String getExtension() {
         return ".tsv";
     }
 
+    /**
+     * Metodo che ritorna il percorso di default in cui bisognerebbe salvare il file.
+     * In questo caso sarebbe la cartella archive/tsv.
+     * @return stringa contenente il percorso in cui effettuare i salvataggi.
+     */
     @Override
     protected String getDefaultPath() {
         return (System.getProperty("user.dir").endsWith("class")) ? "../archive/tsv" : "./archive/tsv";
